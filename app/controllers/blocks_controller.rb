c64b63b7362f04d72c42eda03f0d7dcd4dca0790
alexandr <89227049120@mail.ru>
2022-02-16 13:55:02 +0500

    add function edit block

--- app/controllers/blocks_controller.rb
+++ app/controllers/blocks_controller.rb
@@ -3,6 +3,7 @@ class BlocksController < ApplicationController
   end
 
   def edit
+    @block = Block.find params[:id]
   end
 
   def destroy
@@ -11,5 +12,25 @@ class BlocksController < ApplicationController
     @block.destroy
 
     redirect_to chapter_path(@chapter)
+  end
+
+  def update
+    block = Block.find params[:id]
+    if !params[:comment].blank? || !params[:content].blank? || !params[:lind].blank?
+      params_block = {
+        'content' => params[:content],
+        'comment' => params[:comment],
+        'link' => params[:link] }
+
+      block.update(params_block)
+    end
+
+    redirect_to chapter_path(block.chapter)
+  end
+
+  private
+
+  def block_params
+    params.require(:block).permit(:comment, :content, :link)
   end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,5 @@
 Rails.application.routes.draw do
   root "chapters#index"
   resources :chapters, except: [:index]
-  resources :blocks, only: [:create, :edit, :destroy]
+  resources :blocks, only: [:create, :edit, :destroy, :update]
 end
