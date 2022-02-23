ff8d57a0de0320b0bb258894ad60b1c8d71bad7a
Alexandr <89227049120@mail.ru>
2022-02-23 11:10:48 +0500

    change some style

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,6 +1,6 @@
 Chapter.destroy_all
-Chapter.create(id: 1, name: 'esbuild')
-Chapter.create(id: 2, name: 'git')
-Chapter.create(id: 3, name: 'rspec')
-Chapter.create(id: 4, name: 'generator')
-Chapter.create(id: 5, name: 'string record')
+Chapter.create(id: 3001, name: 'esbuild')
+Chapter.create(id: 3002, name: 'git')
+Chapter.create(id: 3003, name: 'rspec')
+Chapter.create(id: 3004, name: 'generator')
+Chapter.create(id: 3005, name: 'string record')
