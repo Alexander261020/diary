8db1a0c33180297cb98ffc6399378b2c41393c70
Alexandr <89227049120@mail.ru>
2022-02-25 13:41:58 +0500

    failed test controller

---
+++ spec/controllers/chapters_controller_spec.rb
@@ -0,0 +1,18 @@
+require 'rails_helper'
+
+RSpec.describe ChaptersController, type: :controller, fast: true do
+  let(:name) {}
+=begin   let(:chapters) do
+    FactoryBot.create(:chapter)
+    FactoryBot.create(:chapter)
+    FactoryBot.create(:chapter)
+  end
+=end
+  # let(:chapter1) { FactoryBot.create(:chapter) }
+
+  context 'checking actions' do
+    it 'check #index' do
+      # get :index
+    end
+  end
+end
